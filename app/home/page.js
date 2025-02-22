4b4775d19fffc08d1a1d3d72e180a6ec6298f684
tyrants666 <akashyadav.iiita@gmail.com>
2025-02-22 19:39:06 +0545

    Home page added

---
+++ app/home/page.js
@@ -0,0 +1,9 @@
+// app/home/page.js
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            <p>This is the content of the home page.</p>
+        </div>
+    );
+}
