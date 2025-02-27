93c04212d942761c81a7efa0b913934ce661f9ca
tyrants666 <akashyadav.iiita@gmail.com>
2025-02-27 08:47:42 +0545

    Blur updated

--- app/layout.js
+++ app/layout.js
@@ -33,22 +33,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <img
-          src="https://m.media-amazon.com/images/M/MV5BMzgzYjM4NTUtOTlhMS00MTJmLTkxZjgtYWY4NjI1ZWRiNGU4XkEyXkFqcGc@._V1_SX300.jpg"
-          alt="title"
-          className="smoothie absolute object-cover object-top h-full w-full z-1"
-        />
-        <span className="blur-overlay block absolute h-full w-full"></span>
-
-        {/* ================= Blur with FIlter ================== */}
-        {/* <div className="!max-h-[100%] !z-[-1] absolute blur-3xl brightness-50 left-0 opacity-90 overflow-hidden pointer-events-none right-0 top-0">
-          <span className=" lazy-load-image-background opacity lazy-load-image-loaded">
-            <img width="100%" height="100%" src="https://m.media-amazon.com/images/M/MV5BZDI1NGU2ODAtNzBiNy00MWY5LWIyMGEtZjUxZjUwZmZiNjBlXkEyXkFqcGc@._V1_SX300.jpg"
-              className="size-full object-cover object-center !select-none shrink-0 transition-all !duration-1000 opacity-100" />
-          </span>
-        </div> */}
+        <div className="blur-3xl absolute h-full w-full">
+          <img
+            src="https://m.media-amazon.com/images/M/MV5BMzgzYjM4NTUtOTlhMS00MTJmLTkxZjgtYWY4NjI1ZWRiNGU4XkEyXkFqcGc@._V1_SX300.jpg"
+            alt="title"
+            className="smoothie object-cover object-top h-full w-full"
+          />
+          <span className="blur-overlay block absolute h-full w-full z-1 top-0 left-0"></span>
+        </div>
 
         {children}
       </body>
