9a6f91dfc3058959939289835578695a352593d9
tyrants666 <akashyadav.iiita@gmail.com>
2025-02-27 10:38:50 +0545

    Href upadated for same page

--- app/page.js
+++ app/page.js
@@ -126,7 +126,6 @@ export default function Home() {
                                     <a
                                         key={movie.imdbID}
                                         href={`https://www.imdb.com/title/${movie.imdbID}/`}
-                                        target='_blank'
                                         className='shadow-custom flex flex-col hover:scale-105 rounded-xl overflow-hidden smoothie relative group min-w-0 shrink-0 grow-0 
                                     basis-[31.7%] sm:basis-[18.4%] lg:basis-[13.24%] xl:basis-[11.65%] 2xl:basis-[10.4%] max-w-[180px] !select-none'
                                         onMouseEnter={() => handleMovieHover(movie.Poster)}
