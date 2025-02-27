ccf5dbc862f0ff6173cca1cbda86bb955849f26a
tyrants666 <akashyadav.iiita@gmail.com>
2025-02-27 11:01:12 +0545

    Shadow and card aspect ratio update

--- app/page.js
+++ app/page.js
@@ -126,16 +126,16 @@ export default function Home() {
                                     <a
                                         key={movie.imdbID}
                                         href={`https://www.imdb.com/title/${movie.imdbID}/`}
-                                        className='shadow-custom flex flex-col hover:scale-105 rounded-xl overflow-hidden smoothie relative group min-w-0 shrink-0 grow-0 
+                                        className='shadow-custom !aspect-[1.45/2] bg-white/10 flex flex-col hover:scale-105 rounded-xl overflow-hidden smoothie relative group min-w-0 shrink-0 grow-0 
                                     basis-[31.7%] sm:basis-[18.4%] lg:basis-[13.24%] xl:basis-[11.65%] 2xl:basis-[10.4%] max-w-[180px] !select-none'
                                         onMouseEnter={() => handleMovieHover(movie.Poster)}
                                         onMouseLeave={() => null} // Optional: reset to default or keep the last hovered image
                                     >
-                                        <span className=''>
+                                        <span className='h-full w-full'>
                                             <img
                                                 src={movie.Poster !== "N/A" ? movie.Poster : "/placeholder.png"}
                                                 alt={movie.Title}
-                                                className="!select-none shrink-0 undefined rounded-xl overflow-hidden"
+                                                className="h-full w-full object-cover !select-none shrink-0 undefined rounded-xl overflow-hidden"
                                             />
                                         </span>
                                     </a>
