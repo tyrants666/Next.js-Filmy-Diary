58dd52110f73292337f59e47aaf9984a7d755f0e
tyrants666 <akashyadav.iiita@gmail.com>
2025-03-15 12:32:29 +0545

    Fix route.js

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -1,4 +1,4 @@
-javascriptCopyimport { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
