ec1a1c3af92a3413186c9ae937ad1b6f5bef9dbe
tyrants666 <akashyadav.iiita@gmail.com>
2025-03-15 13:02:16 +0545

    redirect fix

--- app/login/page.js
+++ app/login/page.js
@@ -20,7 +20,7 @@ export default function Login() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/`,
+          redirectTo: '/',
         },
       })
       
