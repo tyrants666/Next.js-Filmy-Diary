86bbfbd226aabf94e34bb85d4b75d8c591fc8f7e
tyrants666 <akashyadav.iiita@gmail.com>
2025-04-06 10:55:28 +0545

    Babu Rao Loading Icon Added

--- app/page.js
+++ app/page.js
@@ -54,7 +54,22 @@ export default function Home() {
     }, [user, loading, router]);
 
     if (loading) {
-        return <div className="min-h-screen flex items-center justify-center">Please wait while Mr Babu Rao fixes his dhoti...</div>
+        return <div className="min-h-screen flex flex-col items-center justify-center">
+            <Image 
+                src="/images/babu-rao-stickers.png" 
+                alt="Babu Rao" 
+                width={240} 
+                height={250} 
+                className='mb-2'
+                priority
+            />
+                <p className='text-center text-md px-4'>Please wait while Mr Babu Rao fixes his dhoti...
+                    <svg className="animate-spin h-4 w-4 inline-block text-white ms-2 mb-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </p>
+        </div>
     }
 
     if (!user) {
